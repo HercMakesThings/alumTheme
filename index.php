0b2de2c43037bb5593a630234666445420f6b409
Jeremy <hercmakesthings@gmail.com>
2021-10-05 18:00:21 -0400

    first commit for about and skill post loops

--- index.php
+++ index.php
@@ -27,37 +27,56 @@
     <!--Slanted Sections-->
     <div id="slantseccontainer" class="container-fluid">
 
+        <?php 
+        $args = array(
+            'category_name' => 'about-me'
+        );
+
+        $aboutpost = new WP_Query($args);
+
+        if($aboutpost->have_posts()){
+            while($aboutpost->have_posts()){
+                $aboutpost->the_post();?>
 
         <section id="slantedsec1" class="row gx-0 align-items-center justify-content-center">
             <div data-aos="fade-in-right" id="aboutcol1" class="col-sm-7 p-4">
-              <p>INSERT ABOUT ME POST CONTENT HERE Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p><?php the_content();?></p>
               <div data-aos-duration="1500" data-aos="fade-right" class="accentlineleft"></div>
             </div>
             <!--<div class="vl col-sm-1"></div>-->
             <div data-aos="fade-in-right" id="aboutcol2" class="col-sm-4">
-              <h1>About Me</h1>
-              <small>Insert about post title hook here</small>
+              <h1><?php the_title(); ?></h1>
               <div data-aos-duration="1500" data-aos="zoom-in" class="accentlinecenter"></div>
             </div>
         </section>
+        <?php }//end about while loop
+        }//end about if loop
+        wp_reset_postdata();
+        ?>
+
+        <?php 
+        $skillpost = new WP_Query('category_name=skills');
+
+        if($skillpost->have_posts()){
+            while($skillpost->have_posts()){
+                $skillpost->the_post();?>
 
         <section id="slantedsec2" class="row g-0 align-items-center justify-content-center">
           <div data-aos="fade-in-right" id="skillscol1" class="col-sm-5">
-            <h1>Skills</h1>
+            <h1><?php the_title(); ?></h1>
             <!--<span class="fas fa-arrow-alt-from-left"></span>-->
-            <small>Place the skills post title hook here</small>
             <div data-aos-duration="2000" data-aos="zoom-in" class="accentlinecenterdark"></div>
           </div>
           <div data-aos="fade-in-right" id="skillscol2" class="col-sm-6">
-            <ul>
-              <li>Skill list/bars go here</li>
-              <li>Probably through use of a wp plugin</li>
-              <li>list item 3</li>
-              <li>list item 4</li>
-            </ul>
+            <?php the_content(); ?>
             <div data-aos-duration="2000" data-aos="fade-right" class="accentlineleftdark"></div>
           </div>
         </section>
+        
+        <?php }//end skillpost while loop
+        }//end skillpost if statement
+        wp_reset_postdata();
+        ?>
 
     </div><!--SlantedSecContainersEND-->
 

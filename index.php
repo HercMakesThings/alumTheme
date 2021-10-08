b00a01b8a3bc1638946d1a82c7ae6cd3e9c94d88
Jeremy <hercmakesthings@gmail.com>
2021-10-07 21:28:36 -0400

    added topsection theme template part commit

--- index.php
+++ index.php
@@ -3,13 +3,7 @@
 <!--homepagesection start-->
 <section id="homepage">
     <!--landingPage Top Section-->
-    <section id="topsec" class="scrolldemo" style="background-image: url('<?php echo wp_get_attachment_url(get_theme_mod('landingpage_identity_bgm')); ?>')">
-      <div data-aos="zoom-in-right" id="top-title" class="row align-items-center justify-items-center">
-        <img id="top-logo" class="col-sm-6" src="<?php echo wp_get_attachment_url(get_theme_mod('landingpage_identity_logo')); ?>">
-        <h1 class="col-sm-6"><?php echo get_theme_mod('landingpage_identity_title'); ?></h1>
-      </div>
-        <a id="scrollbtn1" href="#topnavbar"><!--<span class="far fa-arrow-alt-circle-down fa-2x"></span>--><span></span></a>
-    </section>
+    <?php get_template_part('topsec'); ?>
     <!--landingPage Top Section END-->
 
     <!--Top Level Navigation-->
---
+++ topsec.php
@@ -0,0 +1,7 @@
+<section id="topsec" class="scrolldemo" style="background-image: url('<?php echo wp_get_attachment_url(get_theme_mod('landingpage_identity_bgm')); ?>')">
+      <div data-aos="zoom-in-right" id="top-title" class="row align-items-center justify-items-center">
+        <img id="top-logo" class="col-sm-6" src="<?php echo wp_get_attachment_url(get_theme_mod('landingpage_identity_logo')); ?>">
+        <h1 class="col-sm-6"><?php echo get_theme_mod('landingpage_identity_title'); ?></h1>
+      </div>
+        <a id="scrollbtn1" href="#topnavbar"><!--<span class="far fa-arrow-alt-circle-down fa-2x"></span>--><span></span></a>
+    </section>
